f7a03aeae557d8cbfb17ff402c27b1f445ebf18a
miguelemosreverte <miguellemos95@gmail.com>
2017-05-03 05:45:41 -0300

    With this commit this module is finished. Moses-API is run as a background daemon inside the docker-machine acrazymachineforacrazyidea that has access to all of the system cores and reserves half the memory of the system. Victory!

--- startMosesAPI.py
+++ startMosesAPI.py
@@ -1,17 +1,19 @@
 import os, subprocess
 
 def startMosesAPI():
-    directory = os.path.abspath("./Moses-API/")
-
+    directory = os.path.abspath("./WinCompatibleMoses/")
+    os.chdir("C:\Program Files\Docker Toolbox")
     commands_to_run_inside_of_docker = "";
+    #get docker ip
+    docker_ip_file = directory.replace('\\','/') + "/docker-ip.txt";
+    commands_to_run_inside_of_docker += "docker-machine ip acrazymachineforacrazyidea > "+docker_ip_file+";";
     #eval docker machine
-    commands_to_run_inside_of_docker += "eval \"$(docker-machine env acrazymachineforacrazyidea)\";";
+    commands_to_run_inside_of_docker += "eval \"$(docker-machine env acrazymachineforacrazyidea)\";";    
     #and build the moses image
-    commands_to_run_inside_of_docker += " docker run -t -p 5000:5000 test1"
+    commands_to_run_inside_of_docker += " docker run -d -p 5000:5000 test1"
     #and remain open
     #commands_to_run_inside_of_docker += ";bash";
 
-    os.chdir("C:\Program Files\Docker Toolbox")
     p = subprocess.Popen(['start.sh', commands_to_run_inside_of_docker], shell=True)
 
 
